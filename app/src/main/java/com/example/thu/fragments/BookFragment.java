42ecfbf5ed01b53e73cce96a90941470da483ee1
Huong Thu <huongthu1302@gmail.com>
2017-06-13 11:55:25 +0700

    Adding Google Maps, get location from Lat, Lng

--- app/src/main/java/com/example/thu/fragments/BookFragment.java
+++ app/src/main/java/com/example/thu/fragments/BookFragment.java
@@ -1,25 +1,52 @@
 package com.example.thu.fragments;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.thu.taxinhanh.MapsActivity;
 import com.example.thu.taxinhanh.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by thu on 6/12/2017.
  * Guide at http://manishkpr.webheavens.com/android-navigation-drawer-example-using-fragments/
  */
 
-public class BookFragment extends Fragment {
+public class BookFragment extends Fragment implements OnMapReadyCallback {
     private boolean isBookAvailable = false;
+
+    private GoogleMap mMap;
+    MapView mMapView;
 
     public static Fragment newInstance(Context context) {
         BookFragment f = new BookFragment();
@@ -28,14 +55,14 @@ public class BookFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_book, null);
+        final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_book, null);
 
-        TextView tvPickUp = (TextView)root.findViewById(R.id.tvPickUp);
-        final TextView tvDropOff = (TextView)root.findViewById(R.id.tvDropOff);
+        TextView tvPickUp = (TextView) root.findViewById(R.id.tvPickUp);
+        final TextView tvDropOff = (TextView) root.findViewById(R.id.tvDropOff);
         tvPickUp.setSelected(true);
         tvDropOff.setSelected(true);
 
-        ImageButton btnClearPickUp = (ImageButton)root.findViewById(R.id.btnClearDropOff);
+        ImageButton btnClearPickUp = (ImageButton) root.findViewById(R.id.btnClearDropOff);
         btnClearPickUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,10 +71,13 @@ public class BookFragment extends Fragment {
             }
         });
 
-        final ImageButton btnBook = (ImageButton)root.findViewById(R.id.btnBook);
+        final ImageButton btnBook = (ImageButton) root.findViewById(R.id.btnBook);
         btnBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Toast.makeText(getContext(), String.valueOf(mMap.getCameraPosition().tilt),Toast.LENGTH_LONG).show();
+
                 if (isBookAvailable) {
                     btnBook.setImageResource(R.drawable.book_visible);
                 } else {
@@ -58,6 +88,94 @@ public class BookFragment extends Fragment {
             }
         });
 
+        mMapView = (MapView) root.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                mMap = map;
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+
+                TextView tvPickUp = (TextView) root.findViewById(R.id.tvPickUp);
+                tvPickUp.setText(getAddress(10.7622739,106.6822471));
+
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(10.7622739,106.6822471);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().tilt(67).target(sydney).zoom(16).build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return root;
+    }
+
+    public String getAddress(double lat, double lng) {
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0) +  ", " + obj.getSubAdminArea();
+            return add;
+            // TennisAppActivity.showDialog(add);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            return "NOT FOUND";
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
