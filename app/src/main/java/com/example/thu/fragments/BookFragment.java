f088e61714fc7ad2bac11c1eac559f5567c121e0
Huong Thu <huongthu1302@gmail.com>
2017-06-16 18:57:28 +0700

    Adding Route Path

--- app/src/main/java/com/example/thu/fragments/BookFragment.java
+++ app/src/main/java/com/example/thu/fragments/BookFragment.java
@@ -2,25 +2,39 @@ package com.example.thu.fragments;
 
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.thu.taxinhanh.MainActivity;
 import com.example.thu.taxinhanh.MapsActivity;
 import com.example.thu.taxinhanh.R;
+import com.example.thu.utils.DirectionFinder;
+import com.example.thu.utils.DirectionFinderListener;
+import com.example.thu.utils.GPSTracker;
+import com.example.thu.utils.Route;
+import com.example.thu.utils.Utils;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
@@ -31,14 +45,20 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -50,40 +70,49 @@ import static android.app.Activity.RESULT_OK;
  * Guide at http://manishkpr.webheavens.com/android-navigation-drawer-example-using-fragments/
  */
 
-public class BookFragment extends Fragment implements OnMapReadyCallback {
+public class BookFragment extends Fragment implements OnMapReadyCallback, DirectionFinderListener {
     private boolean isBookAvailable = false;
     int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     private GoogleMap mMap;
     MapView mMapView;
 
+    private List<Marker> originMarkers = new ArrayList<>();
+    private List<Marker> destinationMarkers = new ArrayList<>();
+    private List<Polyline> polylinePaths = new ArrayList<>();
+    private ProgressDialog progressDialog;
+
+    private static final String[] LOCATION_PERMS = {
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final int LOCATION_REQUEST = 1340;
+
+
     public static Fragment newInstance(Context context) {
         BookFragment f = new BookFragment();
         return f;
     }
 
+    ViewGroup root = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_book, null);
+        root = (ViewGroup) inflater.inflate(R.layout.activity_book, null);
 
-        TextView tvPickUp = (TextView) root.findViewById(R.id.tvPickUp);
+        final TextView tvPickUp = (TextView) root.findViewById(R.id.tvPickUp);
         final TextView tvDropOff = (TextView) root.findViewById(R.id.tvDropOff);
         tvPickUp.setSelected(true);
         tvDropOff.setSelected(true);
 
-        tvDropOff.setOnClickListener(new View.OnClickListener() {
+        Button btnDropOff = (Button) root.findViewById(R.id.btnDropOff);
+
+        btnDropOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent =
-                            new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
-                                    .build(getActivity());
-                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-                } catch (GooglePlayServicesRepairableException e) {
-                    // TODO: Handle the error.
-                } catch (GooglePlayServicesNotAvailableException e) {
-                    // TODO: Handle the error.
-                }
+                new LoadPlaceAutocomplete().execute();
+
+
             }
         });
 
@@ -131,25 +160,71 @@ public class BookFragment extends Fragment implements OnMapReadyCallback {
 
                 // For showing a move to my location button
                 if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
                     return;
                 }
                 mMap.setMyLocationEnabled(true);
 
+                GPSTracker gpsTracker = new GPSTracker(getActivity());
+
                 TextView tvPickUp = (TextView) root.findViewById(R.id.tvPickUp);
                 tvPickUp.setText(getAddress(10.7622739,106.6822471));
+                //tvPickUp.setText(getAddress(gpsTracker.getLatitude(),gpsTracker.getLongitude()));
 
                 // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(10.7622739,106.6822471);
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+                LatLng sydney = new LatLng(gpsTracker.getLatitude(),gpsTracker.getLongitude());
+                //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
 
                 // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().tilt(67).target(sydney).zoom(16).build();
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(15).build();
 
                 mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
 
         return root;
+    }
+
+    private class LoadPlaceAutocomplete extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Intent intent =
+                        new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                                .build(getActivity());
+                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+            } catch (GooglePlayServicesRepairableException e) {
+                // TODO: Handle the error.
+            } catch (GooglePlayServicesNotAvailableException e) {
+                // TODO: Handle the error.
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ((LinearLayout)getActivity().findViewById(R.id.llLoading)).setVisibility(View.GONE);
+                }
+            });
+        }
+
+        @Override
+        protected void onPreExecute() {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ((LinearLayout)getActivity().findViewById(R.id.llLoading)).setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) { }
     }
 
     @Override
@@ -160,6 +235,10 @@ public class BookFragment extends Fragment implements OnMapReadyCallback {
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(getActivity(), data);
                 tvDropOff.setText(place.getAddress());
+                TextView tvPickUp = (TextView) getActivity().findViewById(R.id.tvPickUp);
+
+                sendRequest(tvPickUp.getText().toString(), place.getAddress().toString());
+
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 tvDropOff.setText(getResources().getText(R.string.please_choose_dropoff));
                 tvDropOff.setTypeface(null, Typeface.ITALIC);
@@ -167,6 +246,14 @@ public class BookFragment extends Fragment implements OnMapReadyCallback {
                 // The user canceled the operation.
                 // Do nothing
             }
+        }
+    }
+
+    private void sendRequest(String origin, String destination) {
+        try {
+            new DirectionFinder(this, origin, destination).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
     }
 
@@ -218,5 +305,56 @@ public class BookFragment extends Fragment implements OnMapReadyCallback {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onDirectionFinderStart() {
+
+    }
+
+    @Override
+    public void onDirectionFinderSuccess(List<Route> routes) {
+        //progressDialog.dismiss();
+        polylinePaths = new ArrayList<>();
+        originMarkers = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
+
+        for (Route route : routes) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+            //tvDuration.setText(route.duration.text);
+            //tvDistance.setText(route.distance.text);
+
+            //distance = route.distance.text;
+            String[] km = route.distance.text.split(" ");
+
+            double money = Double.parseDouble(km[0]);
+
+            TextView tvBook = (TextView)getActivity().findViewById(R.id.tvFare);
+            tvBook.setText("Giá cước: " + money * 11000);
+            //mSocket.emit("CUSTOMER_BOOKS","ahihi");
+            //distance = new Distance(route.distance.text,route.distance.value);
+
+            //startAddress = route.startAddress;
+            //endAddress = route.endAddress;
+
+            originMarkers.add(mMap.addMarker(new MarkerOptions()
+                    //.icon(BitmapDescriptorFactory.fromResource(R.drawable.empty_flag_40))
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+            destinationMarkers.add(mMap.addMarker(new MarkerOptions()
+                    //.icon(BitmapDescriptorFactory.fromResource(R.drawable.empty_flag_40))
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions().
+                    geodesic(true).
+                    color(Color.BLUE).
+                    width(10);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add(mMap.addPolyline(polylineOptions));
+        }
     }
 }
